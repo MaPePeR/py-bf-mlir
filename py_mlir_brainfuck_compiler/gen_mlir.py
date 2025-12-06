6a9155d47bbc6e4de786a25c3e1b20fcf8f7827b
MaPePeR <MaPePeR@users.noreply.github.com>
2025-12-06 20:52:55 +0000

    Only import dialects, not individual Ops

--- py_mlir_brainfuck_compiler/gen_mlir.py
+++ py_mlir_brainfuck_compiler/gen_mlir.py
@@ -2,64 +2,52 @@ from typing import TypeAlias
 
 import lark
 from xdsl.builder import Builder
-from xdsl.dialects.builtin import (
-    FunctionType,
-    ModuleOp,
-)
-from xdsl.dialects.func import FuncOp
+from xdsl.dialects import builtin, func
 from xdsl.ir import (
     Block,
     Region,
 )
 from xdsl.rewriter import InsertPoint
 
-from .dialects.brainfuck import (
-    DecrementOp,
-    IncrementOp,
-    InputOp,
-    LoopOp,
-    MoveLeftOp,
-    MoveRightOp,
-    OutputOp,
-)
+from .dialects import brainfuck as bf
 
 AST: TypeAlias = list[lark.Tree | lark.Token]
 
 
 class GenMLIR:
     builder: Builder
-    module: ModuleOp
+    module: builtin.ModuleOp
 
     def __init__(self) -> None:
-        self.module = ModuleOp([])
+        self.module = builtin.ModuleOp([])
         self.builder = Builder(InsertPoint.at_end(self.module.body.blocks[0]))
 
     def gen_main_func(self, ast: AST):
         body = Block()
         body_builder = Builder(InsertPoint.at_end(body))
         self.gen_instructions(body_builder, ast)
-        func_type = FunctionType.from_lists([], [])
-        self.builder.insert(FuncOp("main", func_type, Region(body)))
+        func_type = builtin.FunctionType.from_lists([], [])
+        self.builder.insert(func.FuncOp("main", func_type, Region(body)))
 
     def gen_instructions(self, builder: Builder, ast: AST):
         for op in ast:
             match op:
                 case lark.Token("MOVE_LEFT"):
-                    builder.insert(MoveLeftOp())
+                    builder.insert(bf.MoveLeftOp())
                 case lark.Token("MOVE_RIGHT"):
-                    builder.insert(MoveRightOp())
+                    builder.insert(bf.MoveRightOp())
                 case lark.Token("INCREMENT"):
-                    builder.insert(IncrementOp())
+                    builder.insert(bf.IncrementOp())
                 case lark.Token("DECREMENT"):
-                    builder.insert(DecrementOp())
+                    builder.insert(bf.DecrementOp())
                 case lark.Token("OUTPUT"):
-                    builder.insert(OutputOp())
+                    builder.insert(bf.OutputOp())
                 case lark.Token("INPUT"):
-                    builder.insert(InputOp())
+                    builder.insert(bf.InputOp())
                 case lark.Tree(lark.Token("RULE", "loop"), children):
                     body = Block()
                     body_builder = Builder(InsertPoint.at_end(body))
                     self.gen_instructions(body_builder, children)
-                    builder.insert(LoopOp(regions=[Region(body)]))
+                    builder.insert(bf.LoopOp(regions=[Region(body)]))
                 case other:
                     raise Exception(f"Invalid Token in AST: {other!r}")
