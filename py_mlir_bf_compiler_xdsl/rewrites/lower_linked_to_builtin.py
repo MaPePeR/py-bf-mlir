f5ab1a9554712f69572d08cb6285e670e9fdf0c1
MaPePeR <MaPePeR@users.noreply.github.com>
2026-01-02 12:45:24 +0000

    xdsl: Use ImplicitBuilder instead of `replace_matched_op` with list for lowering linked to builtin

--- py_mlir_bf_compiler_xdsl/rewrites/lower_linked_to_builtin.py
+++ py_mlir_bf_compiler_xdsl/rewrites/lower_linked_to_builtin.py
@@ -1,4 +1,4 @@
-from xdsl.builder import ImplicitBuilder
+from xdsl.builder import Builder, ImplicitBuilder
 from xdsl.context import Context
 from xdsl.dialects import arith, builtin, func, llvm, memref, scf
 from xdsl.dialects.builtin import ModuleOp
@@ -11,7 +11,7 @@ from xdsl.pattern_rewriter import (
     RewritePattern,
     op_type_rewrite_pattern,
 )
-from xdsl.rewriter import InsertPoint, Rewriter
+from xdsl.rewriter import InsertPoint
 
 from ..dialects import linked_brainfuck as linked_bf
 
@@ -36,13 +36,10 @@ class MoveOpLowering(RewritePattern):
             direction_op = arith.AddiOp
         else:
             raise AssertionError("op was not of the expected type.")
-        rewriter.replace_matched_op(
-            [
-                add_op := direction_op(op.index, self.const_one.result),
-                and_op := arith.AndIOp(add_op.result, self.const_size.result),
-            ],
-            [and_op.result],
-        )
+        with ImplicitBuilder(Builder(InsertPoint.before(op))):
+            add_op = direction_op(op.index, self.const_one.result)
+            and_op = arith.AndIOp(add_op.result, self.const_size.result)
+        rewriter.replace_matched_op([], [and_op.result])
 
 
 class IncDecOpLowering(RewritePattern):
@@ -63,20 +60,13 @@ class IncDecOpLowering(RewritePattern):
                 new_op = arith.SubiOp
             case _:
                 raise AssertionError("op has wrong type")
-        rewriter.replace_matched_op(
-            [
-                load_op := memref.LoadOp(
-                    operands=[self.memref, op.operands[0]], result_types=[MEMORY_TYPE]
-                ),
-                change_op := new_op(
-                    load_op.results[0], self.const_one.result, MEMORY_TYPE
-                ),
-                store_op := memref.StoreOp(
-                    operands=[change_op.result, self.memref, op.operands[0]]
-                ),
-            ],
-            [],
-        )
+        with ImplicitBuilder(Builder(InsertPoint.before(op))):
+            load_op = memref.LoadOp(
+                operands=[self.memref, op.operands[0]], result_types=[MEMORY_TYPE]
+            )
+            change_op = new_op(load_op.results[0], self.const_one.result, MEMORY_TYPE)
+            memref.StoreOp(operands=[change_op.result, self.memref, op.operands[0]])
+        rewriter.replace_matched_op([], [])
 
 
 class LoopOpLowering(RewritePattern):
@@ -142,46 +132,40 @@ class OutputInputOpLowering(RewritePattern):
                 ]
             ),
         )
-        rewriter.replace_matched_op(
-            [
-                zero := arith.ConstantOp(
-                    builtin.IntegerAttr(0, builtin.IntegerType(64))
+
+        with ImplicitBuilder(Builder(InsertPoint.before(op))):
+            zero = arith.ConstantOp(builtin.IntegerAttr(0, builtin.IntegerType(64)))
+            one = arith.ConstantOp(builtin.IntegerAttr(1, builtin.IntegerType(64)))
+            cast_memref_op = builtin.UnrealizedConversionCastOp(
+                operands=[self.memref], result_types=[memref_llvm_struct]
+            )
+            cast_index_op = builtin.UnrealizedConversionCastOp(
+                operands=[op.index], result_types=[builtin.i64]
+            )
+            val_op = llvm.ExtractValueOp(
+                builtin.DenseArrayBase.from_list(builtin.i64, [1]),
+                cast_memref_op.results[0],
+                result_type=llvm.LLVMPointerType(),
+            )
+            elementptr_op = llvm.GEPOp(
+                val_op.results[0],
+                [llvm.GEP_USE_SSA_VAL],
+                MEMORY_TYPE,
+                ssa_indices=[cast_index_op.results[0]],
+            )
+            ptr_to_int_op = llvm.PtrToIntOp(elementptr_op.result)
+            llvm.InlineAsmOp(
+                "syscall",
+                "={rax},{rax},{rdi},{rsi},{rdx},~{rcx},~{r11}",
+                (
+                    [one, one, ptr_to_int_op.results[0], one]
+                    if isinstance(op, linked_bf.OutputOp)
+                    else [zero, zero, ptr_to_int_op.results[0], one]
                 ),
-                one := arith.ConstantOp(
-                    builtin.IntegerAttr(1, builtin.IntegerType(64))
-                ),
-                cast_memref_op := builtin.UnrealizedConversionCastOp(
-                    operands=[self.memref], result_types=[memref_llvm_struct]
-                ),
-                cast_index_op := builtin.UnrealizedConversionCastOp(
-                    operands=[op.index], result_types=[builtin.i64]
-                ),
-                val_op := llvm.ExtractValueOp(
-                    builtin.DenseArrayBase.from_list(builtin.i64, [1]),
-                    cast_memref_op.results[0],
-                    result_type=llvm.LLVMPointerType(),
-                ),
-                elementptr_op := llvm.GEPOp(
-                    val_op.results[0],
-                    [llvm.GEP_USE_SSA_VAL],
-                    MEMORY_TYPE,
-                    ssa_indices=[cast_index_op.results[0]],
-                ),
-                ptr_to_int_op := llvm.PtrToIntOp(elementptr_op.result),
-                llvm.InlineAsmOp(
-                    "syscall",
-                    "={rax},{rax},{rdi},{rsi},{rdx},~{rcx},~{r11}",
-                    (
-                        [one, one, ptr_to_int_op.results[0], one]
-                        if isinstance(op, linked_bf.OutputOp)
-                        else [zero, zero, ptr_to_int_op.results[0], one]
-                    ),
-                    has_side_effects=True,
-                    res_types=[builtin.i64],
-                ),
-            ],
-            [],
-        )
+                has_side_effects=True,
+                res_types=[builtin.i64],
+            )
+        rewriter.replace_matched_op([], [])
 
 
 class LowerLinkedToBuiltinBfPass(ModulePass):
@@ -194,54 +178,42 @@ class LowerLinkedToBuiltinBfPass(ModulePass):
     def apply(self, ctx: Context, op: ModuleOp) -> None:
         assert isinstance(op.body.block.first_op, func.FuncOp)
 
-        init_zero_block = Block([], arg_types=[linked_bf.PositionType()])
-
-        Rewriter.insert_op(
-            [
-                const_zero := arith.ConstantOp(
-                    builtin.IntegerAttr(0, linked_bf.PositionType())
-                ),
-                const_one := arith.ConstantOp(
-                    builtin.IntegerAttr(1, linked_bf.PositionType())
-                ),
-                const_zero_ui8 := arith.ConstantOp(
-                    builtin.IntegerAttr(0, MEMORY_TYPE),
-                ),
-                const_one_ui8 := arith.ConstantOp(
-                    builtin.IntegerAttr(1, MEMORY_TYPE),
-                ),
-                const_index_mask := arith.ConstantOp(
-                    builtin.IntegerAttr(MEMORY_SIZE - 1, linked_bf.PositionType())
-                ),
-                const_size := arith.ConstantOp(
-                    builtin.IntegerAttr(MEMORY_SIZE, linked_bf.PositionType())
-                ),
-                memref_op := memref.AllocOp(
-                    [], [], builtin.MemRefType(MEMORY_TYPE, [MEMORY_SIZE])
-                ),
-                scf.ForOp(
-                    const_zero.result,
-                    const_size.result,
-                    const_one.result,
-                    [],
-                    init_zero_block,
-                ),
-            ],
-            InsertPoint.at_start(op.body.block.first_op.body.block),
-        )
-
-        init_zero_block.add_ops(
-            [
+        with ImplicitBuilder(
+            Builder(InsertPoint.at_start(op.body.block.first_op.body.block))
+        ):
+            const_zero = arith.ConstantOp(
+                builtin.IntegerAttr(0, linked_bf.PositionType())
+            )
+            const_one = arith.ConstantOp(
+                builtin.IntegerAttr(1, linked_bf.PositionType())
+            )
+            const_zero_ui8 = arith.ConstantOp(builtin.IntegerAttr(0, MEMORY_TYPE))
+            const_one_ui8 = arith.ConstantOp(builtin.IntegerAttr(1, MEMORY_TYPE))
+            const_index_mask = arith.ConstantOp(
+                builtin.IntegerAttr(MEMORY_SIZE - 1, linked_bf.PositionType())
+            )
+            const_size = arith.ConstantOp(
+                builtin.IntegerAttr(MEMORY_SIZE, linked_bf.PositionType())
+            )
+            memref_op = memref.AllocOp(
+                [], [], builtin.MemRefType(MEMORY_TYPE, [MEMORY_SIZE])
+            )
+            init_zero_for = scf.ForOp(
+                const_zero.result,
+                const_size.result,
+                const_one.result,
+                [],
+                [Block([], arg_types=[linked_bf.PositionType()])],
+            )
+            with ImplicitBuilder(init_zero_block := init_zero_for.body.block):
                 memref.StoreOp(
                     operands=[
                         const_zero_ui8.result,
                         memref_op.results[0],
                         init_zero_block.args[0],
                     ]
-                ),
-                scf.YieldOp(),
-            ]
-        )
+                )
+                scf.YieldOp()
 
         const_one.result.name_hint = "const_one"
         const_one_ui8.result.name_hint = "const_one_ui8"
